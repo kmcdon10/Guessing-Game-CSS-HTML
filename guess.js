04af116387d49588a633f0c04536de1f3fb14712
Kate Mcdonald <katemcdonald39@gmail.com>
2015-07-13 23:53:27 -0500

    Update guess.js

--- guess.js
+++ guess.js
@@ -1,60 +1,70 @@
-var a=null,b=[],c=5,e=document.getElementById("status"),f=!1,g;
-function h(){
-	e.style.display="block";
-	e.innerHTML="You are "+k()
+var answer = null;
+var guessesRemaining = 5;
+var array = [];
+var playerStatus = document.getElementById("status");
+var valid = !1;
+var playersGuess;
+
+function siteStatusFunc(){
+	playerStatus.style.display = "block";
+	playerStatus.innerHTML = "You are " + hotOrColdFunc()
 }
 
-function l(){
-	a=Math.floor(100*Math.random()+1)
+function generateAnswerFunc(){
+	answer = Math.ceil(100 * Math.random());
 }
 
-function m(){
-	l();
-	n(a)
+function startGameFunc(){
+	generateAnswerFunc();
+	checkNumFunc(answer);
 }
 
-function k(){
-	var d=Math.abs(g-a);
-	return 5>=d?"Super Hot, "+p():10>=d?"Hot, "+p():15>=d?"Warm, "+p():25>=d?"Cold, "+p():"Ice Cold, "+p()
+function hotOrColdFunc(){
+	var difference = Math.abs(playersGuess - answer);
+	return 5 >= difference ? "Super Hot, " + hiLoFunc() : 10 >= difference ? "Hot, "+ hiLoFunc() : 15 >= difference ? "Warm, "+ hiLoFunc() : 25 >= difference ? "Cold, " + hiLoFunc() : "Ice Cold, " + hiLoFunc();
 }
 
-function p(){
-	return g>a?"Guess Lower":"Guess Higher"
+function hiLoFunc(){
+	return playersGuess > answer ? "Guess Lower" : "Guess Higher";
 }
 
-function n(d){
-	q();
-	isNaN(g)||100<g||1>g||f?f?(e.style.display="block",e.innerHTML="You already picked that number"):alert("This is not a valid number"):parseInt(g,10)===parseInt(d,10)?(e.style.display="block",e.innerHTML="You Are CORRECT!"):(0===b.length?b=[g]:b.push(g),h(),r())
+function checkNumFunc(difference){
+	isDuplicateFunc();
+	isNaN(playersGuess) || 100<playersGuess || 1>playersGuess ||valid?valid?(playerStatus.style.display="block",playerStatus.innerHTML="You already picked that number") : alert("This is not a valid number") : parseInt(playersGuess,10) === parseInt(difference,10) ? (playerStatus.style.display="block",playerStatus.innerHTML="You Are CORRECT!") : (0===array.length ? array=[playersGuess] : array.push(playersGuess),siteStatusFunc(),guessesLeftFunc());
 }
 
-function q(){
-	f=!1;
-	b.forEach(function(d){
-		parseInt(g,10)===parseInt(d,10)&&(f=!0)
+function isDuplicateFunc(){
+	valid =! 1;
+	array.forEach(function(difference){
+		parseInt(playersGuess,10) === parseInt(difference,10) && (valid =! 0);
 	})
 }
 
-function r(){
-	2<c?(c-=1,document.getElementById("guess-count").innerHTML=c+" Guesses Remaining"):2===c?(c-=1,document.getElementById("guess-count").innerHTML=c+" Guess Remaining"):document.getElementById("guess-count").innerHTML="Sorry, Play Again!"
+function guessesLeftFunc(){
+	2 < guessesRemaining ? (guessesRemaining -= 1,document.getElementById("guess-count").innerHTML=guessesRemaining+" Guesses Remaining") : 2 === guessesRemaining ? (guessesRemaining -= 1,document.getElementById("guess-count").innerHTML=guessesRemaining+" Guess Remaining"):document.getElementById("guess-count").innerHTML="Sorry, Play Again!"
 }
 
 window.doesNumberToGuessExist=function(){
-		g=parseInt(document.getElementById("playersGuess").value,10);null===a?(m(),document.getElementById("playersGuess").value=""):(document.getElementById("playersGuess").value="",n(a))
+		playersGuess = parseInt(document.getElementById("playersGuess").value,10);
+		null === answer ? (startGameFunc(),document.getElementById("playersGuess").value="") : (document.getElementById("playersGuess").value="",checkNumFunc
+		(answer));
 };
 
-window.status=h;
-window.createNumberToGuess=l;
-window.startGame=m;
-window.hotOrCold=k;window.higherOrLower=p;
-window.isItTheNumber=n;
-window.checkDuplicate=q;
-window.guessesRemaining=r;
+window.status = siteStatusFunc;
+window.createNumberToGuess = generateAnswerFunc;
+window.startGame = startGameFunc;
+window.hotOrCold = hotOrColdFunc;
+window.higherOrLower = hiLoFunc;
+window.isItTheNumber = checkNumFunc;
+window.checkDuplicate = isDuplicateFunc;
+window.guessesRemaining = guessesLeftFunc;
 window.playAgain=function(){
-	a=null;c=5;b=[];
-	e.innerHTML="Your game has been restarted, submit a new guess!";
-	g=void 0;
-	document.getElementById("guess-count").innerHTML=c+" Guesses Remaining";document.getElementById("playersGuess").value=""
+	answer=null;guessesRemaining=5;array=[];
+	playerStatus.innerHTML="The game has been restarted, submit a new guess!";
+	playersGuess=void 0;
+	document.getElementById("guess-count").innerHTML=guessesRemaining+" Guesses Remaining";
+	document.getElementById("playersGuess").value="";
 };
 window.hint=function(){
-	void 0===g?alert("Please submit a guess first!"):document.getElementById("guess-count").innerHTML=a
+	void 0 === playersGuess ? alert("Please submit a guess first!") : document.getElementById("guess-count").innerHTML=answer
 };
